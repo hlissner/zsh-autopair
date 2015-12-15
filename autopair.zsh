bdbcc9c429ec755edc9b07eea725154216db8c82
Henrik Lissner <henrik@lissner.net>
2015-12-15 05:43:23 -0500

    Complete rewrite

--- autopair.zsh
+++ autopair.zsh
@@ -1,113 +1,137 @@
 # A widget that auto-inserts matching pairs in ZSH.
 
-# TODO Option for allowed/restricted pairing boundaries
-# TODO Option for auto-init keybinds
+# Stops zsh-autopair from auto-binding keymaps
+AUTOPAIR_INHIBIT_INIT=
+AUTOPAIR_BETWEEN_WHITESPACE=
 
-_autopair-get-pair() {
-    if [[ -n "$1" ]]
-    then case "$1" in
-             '"') echo -n '"' ;;
-             "'") echo -n "'" ;;
-             '`') echo -n '`' ;;
-             '(') echo -n ')' ;;
-             '[') echo -n ']' ;;
-             '{') echo -n '}' ;;
-             '<') echo -n '>' ;;
-             *) echo -n "";;
-         esac
-    elif [[ -n "$2" ]]
-    then case "$2" in
-             ')') echo -n '(' ;;
-             ']') echo -n '[' ;;
-             '}') echo -n '{' ;;
-             '>') echo -n '<' ;;
-             *) echo -n "" ;;
-         esac
+typeset -A AUTOPAIR_PAIRS
+AUTOPAIR_PAIRS=('`' '`' "'" "'" '"' '"' '{' '}' '[' ']' '(' ')' '<' '>')
+
+typeset -A AUTOPAIR_LBOUNDS
+AUTOPAIR_LBOUNDS=(\
+    all '[.:/\!]' \
+    quotes '[]})a-zA-Z0-9]' \
+    braces '' \
+    '"' '"' \
+    "'" "'" \
+    '`' '`')
+
+typeset -A AUTOPAIR_RBOUNDS
+AUTOPAIR_RBOUNDS=(\
+    all '[[{(<,.:?/%$!a-zA-Z0-9]' \
+    quotes '[a-zA-Z0-9]' \
+    braces '')
+
+####
+
+ap-boundary-p() {
+    [[ -n "$1" && "$LBUFFER" =~ "${1}$" || -n "$2" && "$RBUFFER" =~ "^${2}" ]]
+}
+ap-next-to-boundary-p() {
+    local groups=(all)
+    case "$1" in
+        \'|\"|\`)    groups+=quotes ;;
+        \{|\[|\(|\<) groups+=braces ;;
+    esac
+    groups+="$1"
+    for group in $groups; do
+        ap-boundary-p "$AUTOPAIR_LBOUNDS[$group]" "$AUTOPAIR_RBOUNDS[$group]" && return 0
+    done
+    return 1
+}
+
+# If provided pair is balanced in the buffer
+ap-balanced-p() {
+    local llen="${#LBUFFER//[^$1]}"
+    local rlen="${#RBUFFER//[^$2]}"
+    if [[ -n "$llen" && -n "$rlen" ]]
+    then
+        (( $llen == $rlen )) || \
+            (( $llen % 2 == 0 && $rlen % 2 == 0 )) || \
+            (( $(($llen>$rlen?$llen - $rlen:$rlen - $llen)) == 0 ))
     fi
 }
 
-_autopair-balanced-p() {
-    local l=$(_autopair-count $([[ $1 == $2 ]] && echo "$LBUFFER" || echo "$BUFFER") "$1")
-    local r=$(_autopair-count $([[ $1 == $2 ]] && echo "$RBUFFER" || echo "$BUFFER") "$2")
-    [[ -z $1 || -z $r ]] && return 1
-    [[ $l == $r || $(( ($l + $r) % 2 == 0 )) ]] && return 0
-}
-
-_autopair-count() {
-    expr $(echo "$1" | fgrep -o "${2/-/\-}" - | wc -l ) + 0
-}
-
-_autopair-pair-p() {
-    local rchar=$(_autopair-get-pair $KEYS)
+ap-can-pair-p() {
+    local rchar="$AUTOPAIR_PAIRS[$KEYS]"
 
     # Don't pair if pair doesn't exist
     [[ -z "$rchar" ]] && return 1
-    # Don't pair if the delimiters are unbalanced
-    ! _autopair-balanced-p "$KEYS" "$rchar" && return 1
-    # Don't pair when in front of characters that likely signify the
-    # start of a string or path.
-    [[ $LBUFFER[-1] == '!' || "$RBUFFER" =~ "^[[{(<a-zA-Z0-9!$%/,.:?]" ]] && return 1
-    # For quotes...
-    if [[ "$KEYS" == (\'|\"|\`) ]]
-    then
-        # Don't pair if after yourself
-        [[ $LBUFFER[-1] == "$KEYS" ]] && return 1
-        # Don't pair if next to alphanumerics or after closing brackets
-        [[ "$LBUFFER" =~ "[]a-zA-Z0-9)}]$" ]] && return 1
-    else # For braces
-        # Don't pair if next to the same starting delimiter
-        [[ $RBUFFER[1] == $KEYS ]] && return 1
-    fi
-    # Pair if surrounded by boundaries
-    [[ "$LBUFFER" =~ "(^|[ 	])$" && "$RBUFFER" =~ "^($|[ 	])" ]] && return 0
+
+    # Force pair if surrounded by space/[BE]OL, regardless of boundaries/balance
+    [[ -n "$AUTOPAIR_BETWEEN_WHITESPACE" && \
+        "$LBUFFER" =~ "(^|[ 	])$" && \
+        "$RBUFFER" =~ "^($|[ 	])" ]] && return 0
+
+    # Don't pair quotes if the delimiters are unbalanced
+    ! ap-balanced-p $KEYS $rchar && return 1
+
+    # Don't pair when in front of characters that likely signify the start of a string
+    # or path (i.e. boundary characters)
+    ap-next-to-boundary-p "$KEYS" "$rchar" && return 1
 
     return 0
 }
 
-_autopair-skip-p() {
-    [[ -n "$2" && $RBUFFER[1] == $2 ]] && _autopair-balanced-p "$1" "$2"
+ap-can-skip-p() {
+    [[ -n "$2" && "$RBUFFER[1]" == "$2" ]] # && ap-balanced-p "$1" "$2"
 }
 
-_autopair-delete-p() {
-    local lchar=$LBUFFER[-1]
-    local rchar=$(_autopair-get-pair "$lchar")
-    [[ -n "$rchar" && $RBUFFER[1] == $rchar ]] && _autopair-balanced-p "$lchar" "$rchar"
+ap-can-delete-p() {
+    local lchar="$LBUFFER[-1]"
+    local rchar="$AUTOPAIR_PAIRS[$lchar]"
+    [[ -n "$rchar" && "$RBUFFER[1]" == "$rchar" ]] && ap-balanced-p "$lchar" "$rchar"
 }
 
-_autopair-self-insert() {
-    LBUFFER+="$1"
-    RBUFFER="$2$RBUFFER"
+typeset -A AUTOPAIR_REVERSE_PAIRS
+for i in ${(@k)AUTOPAIR_PAIRS}; do AUTOPAIR_REVERSE_PAIRS["$AUTOPAIR_PAIRS[$i]"]="$i"; done
+typeset -r AUTOPAIR_REVERSE_PAIRS
+
+autopair-self-insert() {
+    LBUFFER+="$1$2"
+    zle backward-char
 }
 
 autopair-insert() {
-    if _autopair-pair-p
-    then _autopair-self-insert "$KEYS" $(_autopair-get-pair $KEYS)
-    else zle self-insert
-    fi
-}
-
-autopair-insert-or-skip() {
-    local rchar=$(_autopair-get-pair $KEYS)
-    if _autopair-skip-p "$KEYS" "$rchar"
-    then zle forward-char
+    local rchar="$AUTOPAIR_PAIRS[$KEYS]"
+    if [[ $KEYS == (\'|\"|\`) ]] && ap-can-skip-p "$KEYS" "$rchar"; then
+        zle forward-char
+    elif ap-can-pair-p; then
+        autopair-self-insert "$KEYS" "$rchar"
     else
-        _autopair-pair-p && _autopair-self-insert "$KEYS" "$rchar" || zle self-insert
+        zle self-insert
     fi
 }
 
-autopair-skip() {
-    if _autopair-skip-p $(_autopair-get-pair "" "$KEYS") "$KEYS"
+autopair-close() {
+    if ap-can-skip-p "$AUTOPAIR_REVERSE_PAIRS[$KEYS]" "$KEYS"
     then zle forward-char
     else zle self-insert
     fi
 }
 
 autopair-delete() {
-    _autopair-delete-p && zle delete-char
+    ap-can-delete-p && zle delete-char
     zle backward-delete-char
 }
 
-zle -N autopair-insert
-zle -N autopair-insert-or-skip
-zle -N autopair-skip
-zle -N autopair-delete
+# Initialization
+[[ -z "$AUTOPAIR_INHIBIT_INIT" ]] && {
+    for i in ${(@k)AUTOPAIR_PAIRS}; do
+        bindkey "$i" autopair-insert
+        bindkey -M isearch "$i" self-insert
+    done
+
+    local l=(')' '}' ']' '>')
+    for i in $l; do
+        bindkey "$i" autopair-close
+        bindkey -M isearch "$i" self-insert
+    done
+
+    bindkey "^?" autopair-delete
+    bindkey -M isearch "^?" backward-delete-char
+
+    zle -N autopair-insert
+    zle -N autopair-close
+    zle -N autopair-delete
+}
