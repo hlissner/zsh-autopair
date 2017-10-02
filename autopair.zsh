3361fc6f4a2ef362c9e55fa1adecf682754e03e4
Henrik Lissner <henrik@lissner.net>
2017-10-02 15:34:52 +0200

    Refactor; remove unnecessary quoting

--- autopair.zsh
+++ autopair.zsh
@@ -27,31 +27,32 @@ AUTOPAIR_RBOUNDS[braces]=
 ####
 
 ap-get-pair() {
-    if [[ -n "$1" ]]; then
-        echo "${AUTOPAIR_PAIRS[$1]}"
-    elif [[ -n "$2" ]]; then
+    if [[ $1 ]]; then
+        echo $AUTOPAIR_PAIRS[$1]
+    elif [[ $2 ]]; then
         local i
         for i in ${(@k)AUTOPAIR_PAIRS}; do
-            [[ "$2" == "${AUTOPAIR_PAIRS[$i]}" ]] && echo "$i" && break
+            [[ $2 == $AUTOPAIR_PAIRS[$i] ]] && echo $i && break
         done
     fi
 }
 
 ap-boundary-p() {
-    [[ -n "$1" && "$LBUFFER" =~ "$1$" ]] || [[ -n "$2" && "$RBUFFER" =~ "^$2" ]]
+    [[ -n $1 && $LBUFFER =~ "$1$" ]] || [[ -n $2 && $RBUFFER =~ "^$2" ]]
 }
+
 ap-next-to-boundary-p() {
     local -a groups
     groups=(all)
-    case "$1" in
+    case $1 in
         \'|\"|\`)    groups+=quotes ;;
         \{|\[|\(|\<) groups+=braces ;;
         " ")         groups+=spaces ;;
     esac
-    groups+="$1"
+    groups+=$1
     local group
     for group in $groups; do
-        ap-boundary-p "$AUTOPAIR_LBOUNDS[$group]" "$AUTOPAIR_RBOUNDS[$group]" && return 0
+        ap-boundary-p $AUTOPAIR_LBOUNDS[$group] $AUTOPAIR_RBOUNDS[$group] && return 0
     done
     return 1
 }
@@ -62,85 +63,93 @@ ap-balanced-p() {
     local rbuf="${RBUFFER//\\$2}"
     local llen="${#lbuf//[^$1]}"
     local rlen="${#rbuf//[^$2]}"
-    (( $rlen == 0 && $llen == 0 )) && return 0
-    if [[ "$1" == "$2" ]]; then
-        if [[ "$1" == " " ]]; then
+    if (( rlen == 0 && llen == 0 )); then
+        return 0
+    elif [[ $1 == $2 ]]; then
+        if [[ $1 == " " ]]; then
             # Silence WARN_CREATE_GLOBAL errors
             local match=
             local mbegin=
             local mend=
-            [[ "$LBUFFER" =~ "[^'\"]([ 	]+)$" && "$RBUFFER" =~ "^$match[1]" ]] && return 0
+            [[ $LBUFFER =~ "[^'\"]([ 	]+)$" && $RBUFFER =~ "^${match[1]}" ]] && return 0
             return 1
-        elif (( $llen == $rlen || ($llen + $rlen) % 2 == 0 )); then
+        elif (( llen == rlen || (llen + rlen) % 2 == 0 )); then
             return 0
         fi
     else
         local l2len="${#lbuf//[^$2]}"
         local r2len="${#rbuf//[^$1]}"
-        local ltotal=$(( $llen - $l2len ))
-        local rtotal=$(( $rlen - $r2len ))
+        local ltotal=$((llen - l2len))
+        local rtotal=$((rlen - r2len))
 
-        (( $ltotal < 0 )) && ltotal=0
-        (( $ltotal < $rtotal )) && return 1
+        (( ltotal < 0 )) && ltotal=0
+        (( ltotal < rtotal )) && return 1
         return 0
     fi
     return 1
 }
 
 ap-can-pair-p() {
-    local rchar=$(ap-get-pair "$KEYS")
+    local rchar=$(ap-get-pair $KEYS)
 
     # Don't pair if pair doesn't exist
-    [[ -z "$rchar" ]] && return 1
+    [[ $rchar ]] || return 1
 
     # Don't pair quotes if the delimiters are unbalanced
-    if [[ "$rchar" != " " ]]; then
+    if [[ $rchar != " " ]]; then
         # Force pair if surrounded by space/[BE]OL, regardless of
         # boundaries/balance
-        [[ -n "$AUTOPAIR_BETWEEN_WHITESPACE" && \
-            "$LBUFFER" =~ "(^|[ 	])$" && \
-            "$RBUFFER" =~ "^($|[ 	])" ]] && return 0
+        [[ $AUTOPAIR_BETWEEN_WHITESPACE && \
+            $LBUFFER =~ "(^|[ 	])$" && \
+            $RBUFFER =~ "^($|[ 	])" ]] && return 0
 
         # Don't pair quotes if the delimiters are unbalanced
-        ! ap-balanced-p "$KEYS" "$rchar" && return 1
-    elif [[ "$RBUFFER" =~ "^[ 	]*$" ]]; then
+        ! ap-balanced-p $KEYS $rchar && return 1
+    elif [[ $RBUFFER =~ "^[ 	]*$" ]]; then
         return 1
     fi
 
     # Don't pair when in front of characters that likely signify the start of a
     # string or path (i.e. boundary characters)
-    ap-next-to-boundary-p "$KEYS" "$rchar" && return 1
+    ap-next-to-boundary-p $KEYS $rchar && return 1
 
     return 0
 }
 
 ap-can-skip-p() {
-    [[ "$1" == " " && "$1" == "$2" ]] && return 1
-    ! [[ -n "$2" && "$RBUFFER[1]" == "$2" && "$LBUFFER[-1]" != '\' ]] && return 1
-    [[ "$1" == "$2" ]] && ! ap-balanced-p "$1" "$2" && return 1
+    if [[ $1 == $2 ]]; then
+        if [[ $1 == " " ]]; then
+            return 1
+        elif ! ap-balanced-p $1 $2; then
+            return 1
+        fi
+    fi
+    if ! [[ $2 && $RBUFFER[1] == $2 && $LBUFFER[-1] != '\' ]]; then
+        return 1
+    fi
     return 0
 }
 
 ap-can-delete-p() {
     local lchar="$LBUFFER[-1]"
-    local rchar=$(ap-get-pair "$lchar")
-    ! [[ -n "$rchar" && "$RBUFFER[1]" == "$rchar" ]] && return 1
-    [[ "$lchar" == "$rchar" ]] && ! ap-balanced-p "$lchar" "$rchar" && return 1
+    local rchar=$(ap-get-pair $lchar)
+    ! [[ $rchar && $RBUFFER[1] == $rchar ]] && return 1
+    [[ $lchar == $rchar ]] && ! ap-balanced-p $lchar $rchar && return 1
     return 0
 }
 
 autopair-self-insert() {
-    LBUFFER+="$1$2"
+    LBUFFER+=$1$2
     zle backward-char
 }
 
 autopair-insert() {
-    local rchar=$(ap-get-pair "$KEYS")
-    if [[ "$KEYS" == (\'|\"|\`| ) ]] && ap-can-skip-p "$KEYS" "$rchar"; then
+    local rchar=$(ap-get-pair $KEYS)
+    if [[ $KEYS == (\'|\"|\`| ) ]] && ap-can-skip-p $KEYS $rchar; then
         zle forward-char
     elif ap-can-pair-p; then
-        autopair-self-insert "$KEYS" "$rchar"
-    elif [[ "$rchar" == " " && -n "$AUTOPAIR_SPC_WIDGET" ]]; then
+        autopair-self-insert $KEYS $rchar
+    elif [[ $rchar == " " && $AUTOPAIR_SPC_WIDGET ]]; then
         zle $AUTOPAIR_SPC_WIDGET
     else
         zle self-insert
@@ -148,7 +157,7 @@ autopair-insert() {
 }
 
 autopair-close() {
-    if ap-can-skip-p $(ap-get-pair "" "$KEYS") "$KEYS"
+    if ap-can-skip-p $(ap-get-pair "" $KEYS) $KEYS
     then zle forward-char
     else zle self-insert
     fi
@@ -183,4 +192,4 @@ autopair-init() {
     bindkey -M isearch "^?" backward-delete-char
     bindkey -M isearch "^h" backward-delete-char
 }
-[[ -z "$AUTOPAIR_INHIBIT_INIT" ]] && autopair-init
+[[ $AUTOPAIR_INHIBIT_INIT ]] || autopair-init
