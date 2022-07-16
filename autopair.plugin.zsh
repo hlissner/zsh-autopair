aad60973e109f429a3cd67700790876a5afd582a
Wu Zhenyu <wuzy01@qq.com>
2022-07-16 11:44:30 +0800

    Fix #26, change symbolic links to files

--- autopair.plugin.zsh
+++ autopair.plugin.zsh
@@ -1,1 +1,4 @@
-autopair.zsh
+# <https://github.com/zdharma-continuum/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc>
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+source "${0:A:h}/autopair.zsh"
